eb4475b9bfb10d10845cf4164d6b202f80f8e7a2
Vadim Mostovenko <mostovenko.vadim@wb.ru>
2024-01-10 22:57:46 +0200

    Добавлен сервис для storage

--- weather.js
+++ weather.js
@@ -1,6 +1,16 @@
 #!/usr/bin/env node
 import { getArgs } from './helpers/args.js'
 import { printError, printHelp, printSuccess } from './services/log.service.js';
+import { setKeyValue } from './services/storage.service.js';
+
+const saveToken = async (token) => {
+	try {
+		await setKeyValue('token', token)
+		printSuccess('Токен сохранен');
+	} catch (e) {
+		printError(e.message)
+	}
+}
 
 const initCLI = () => {
 	const args = getArgs(process.argv)
@@ -8,10 +18,9 @@ const initCLI = () => {
 		printHelp()
 	}
 	if (args.s) {
-		// Сохранить город
 	}
 	if (args.t) {
-		// Сохранить токен
+		return saveToken(args.t)
 	}
 	// Вывести погоду
 }
