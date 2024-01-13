7add622c4376d88029516eb3cf95b3773f363e9a
Vadim Mostovenko <mostovenko.vadim@wb.ru>
2024-01-14 00:18:25 +0200

    Добавлена обработка ошибок

--- services/api.service.js
+++ services/api.service.js
@@ -2,13 +2,13 @@ import { getKeyValue, TOKEN_DECTIONARY } from './storage.service.js';
 import axios from 'axios';
 
 const getWeather = async (city) => {
-	const token = await getKeyValue(TOKEN_DECTIONARY.token);
+	const token = await getKeyValue(TOKEN_DECTIONARY.token) ?? process.env.TOKEN;
 	if (!token) {
 		throw new Error('Не задан ключ API, задайте его через команду -t [API_KEY]')
 	}
 
 	const { data } = await axios.get('https://api.openweathermap.org/data/2.5/weather', { params: {q: city, appid: token, lang: 'ru', units: 'metrics'}})
-	console.log(data)
+	return data
 }
 
 export { getWeather };
